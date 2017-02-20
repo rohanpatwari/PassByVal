b2c3d99b38c3c5b34b46f00f3af0ece1d77f2577
rohan <rohan@DESKTOP-2911EVS>
2017-02-20 14:31:06 -0600

    Add project files.

---
+++ PassByValueApplication/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassByValueApplication
+{
+    class Square
+    {
+        public void PerimeterSq(int a, int b)
+        {
+            a = 4 * a;
+            b = 4 * b;
+            Console.WriteLine(a + " " + b);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 4;
+            int num2 = 5;
+            Console.WriteLine(num1 + " " + num2);
+            Square s = new Square();
+            s.PerimeterSq(num1, num2);
+            Console.WriteLine(num1 + " " + num2);
+            Console.ReadKey();
+        }
+    }
+}
